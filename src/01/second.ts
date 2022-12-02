4718e63add9d34e6b92136ce45a2114d3e2ecfb4
JC Olivares <juancri@juancri.com>
2022-12-02 13:58:12 -0600

    Formatting code

--- src/01/second.ts
+++ src/01/second.ts
@@ -1,7 +1,8 @@
 
 import InputFile from '../util/InputFile';
 
-console.log(InputFile.readIntGroups('./input/01/input.txt')
+console.log(InputFile
+	.readIntGroups('./input/01/input.txt')
 	.select(group => group.sum())
 	.orderByDescending(x => x)
 	.take(3)
