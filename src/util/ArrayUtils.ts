789137f3191d07bf56eb6b0c8c83acbaaee955cf
JC Olivares <juancri@juancri.com>
2022-12-02 23:48:44 -0600

    Adding ArrayUtils.getFirst

--- src/util/ArrayUtils.ts
+++ src/util/ArrayUtils.ts
@@ -1,6 +1,13 @@
 
 export default class ArrayUtils
 {
+	public static getFirst<T>(a: T[]): T
+	{
+		if (a.length === 0)
+			throw new Error('No items found in array');
+		return a[0] as T;
+	}
+
 	public static getRepeatedItems<T>(lists: T[][]): T[]
 	{
 		if (lists.length == 0)
