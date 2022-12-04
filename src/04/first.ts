c83623594a50530bbbff813ca6e70ce417468db1
JC Olivares <juancri@juancri.com>
2022-12-03 23:52:46 -0600

    Improving code formatting

--- src/04/first.ts
+++ src/04/first.ts
@@ -1,13 +1,15 @@
 
 import InputFile from '../util/InputFile';
 
+type Pair = [number, number];
+type PairOfPairs = [Pair, Pair];
+
 console.log(InputFile
 	.readLinesForDay(4)
 	.select(line => line.split(','))
-	.select(pairs => pairs.map(pair =>
-		pair.split('-').map(x => parseInt(x))))
+	.select(pairs => pairs.map(pair => pair
+		.split('-')
+		.map(x => parseInt(x)) as Pair) as PairOfPairs)
 	.count(x =>
-		// @ts-ignore
 		(x[0][0] >= x[1][0] && x[0][1] <= x[1][1]) ||
-		// @ts-ignore
 		(x[1][0] >= x[0][0] && x[1][1] <= x[0][1])));
--- src/04/second.ts
+++ src/04/second.ts
@@ -1,10 +1,13 @@
 
 import InputFile from '../util/InputFile';
 
+type Pair = [number, number];
+type PairOfPairs = [Pair, Pair];
+
 console.log(InputFile
 	.readLinesForDay(4)
 	.select(line => line.split(','))
-	.select(pairs => pairs.map(pair =>
-		pair.split('-').map(x => parseInt(x))))
-	// @ts-ignore
+	.select(pairs => pairs.map(pair => pair
+		.split('-')
+		.map(x => parseInt(x)) as Pair) as PairOfPairs)
 	.count(x => x[0][0] <= x[1][1] && x[0][1] >= x[1][0]));
