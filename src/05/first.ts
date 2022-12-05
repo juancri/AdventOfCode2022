e8374a9dae44b8c452fb04d09a529704d85a7026
JC Olivares <juancri@juancri.com>
2022-12-05 02:04:52 -0600

    Simplifying code

--- src/05/first.ts
+++ src/05/first.ts
@@ -4,51 +4,37 @@ import InputFile from '../util/InputFile';
 const data = InputFile
 	.readFileForDay(5)
 	.split('\n\n')
+	.map(g => g.split('\n'))
 	.toPair();
+
 const stacks = data.first
-	.split('\n')
 	.skipLast()
-	.map(line => line.split(''))
-	.map(chars => chars.filter((_c, index) => (index - 1) % 4 === 0))
-	.map(crates => crates.map((crate, index) => ({ crate, stack: index + 1 })))
-	.map(crates => crates.filter(x => x.crate.trim().length > 0))
 	.toEnumerable()
+	.select(line => line.split(''))
+	.select(chars => chars.filter((_c, index) => (index - 1) % 4 === 0))
+	.select(crates => crates.map((crate, index) => ({ crate, index })))
+	.select(crates => crates.filter(x => x.crate !== ' '))
 	.selectMany(crates => crates)
-	.groupBy(x => x.stack)
-	.select(g => ({
-		stack: g.first().stack,
-		crates: g.reverse()
-			.select(x => x.crate)
-			.toArray()
-	}))
-	.orderBy(s => s.stack)
+	.groupBy(x => x.index)
+	.orderBy(g => g.first().index)
+	.select(g => g.reverse()
+		.select(x => x.crate)
+		.toArray())
 	.toArray();
-const moves = data.second
-	.split('\n')
-	.filter(x => x.length !== 0)
+
+data.second
+	.filter(line => line.length > 0)
 	.map(line => /^move (\d+) from (\d+) to (\d+)$/.exec(line))
 	.map(a => (a as string[]).slice(1, 4))
 	.map(a => a.toIntArray())
-	.map(a => ({ count: a[0] || 0, from: a[1], to: a[2] }))
-
-for (const move of moves)
-{
-	const from = stacks.find(s => s.stack === move.from);
-	const to = stacks.find(s => s.stack === move.to);
-	while (move.count-- > 0)
+	.forEach(move =>
 	{
-		const crate = from?.crates.pop();
-		if (!crate)
-			throw new Error(`Crate not found for stack ${move.from}`);
-		to?.crates.push(crate);
-	}
-}
+		const from = stacks.atCheck(move.atCheck(1) - 1);
+		stacks
+			.atCheck(move.atCheck(2) - 1)
+			.push(...from.spliceFromLast(move.atCheck(0)).reverse());
+	});
 
 console.log(stacks
-	.map(stack => stack.crates
-		.toEnumerable()
-		.last())
-	.toEnumerable()
-	.selectMany(x => x)
-	.toArray()
+	.flatMap(stack => stack.getLast())
 	.join(''));
