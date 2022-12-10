3b7a90e7603d8ce8fe8a3835273a35fb0fbdaf74
JC Olivares <juancri@juancri.com>
2022-12-09 21:22:57 -0600

    Adding method ArrayUtils.createWith(size, getValue)

--- src/util/ArrayUtils.ts
+++ src/util/ArrayUtils.ts
@@ -1,6 +1,13 @@
 
 export default class ArrayUtils
 {
+	public static createWith<T>(size: number, getValue: () => T): T[]
+	{
+		return Array(size)
+			.fill(null)
+			.map(() => getValue());
+	}
+
 	public static getRepeatedItems<T>(lists: T[][]): T[] {
 		return (lists[0] ?? []).filter(item =>
 			lists.every(list => list.includes(item)));
