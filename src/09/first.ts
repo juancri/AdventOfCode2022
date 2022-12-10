3c6ea1a81604b9175390a652e5cc34663e558fdd
JC Olivares <juancri@juancri.com>
2022-12-10 00:21:14 -0600

    Optimizing code

--- src/09/first.ts
+++ src/09/first.ts
@@ -1,46 +1,41 @@
 
-import Enumerable from 'linq';
 import ArrayUtils from '../util/ArrayUtils';
 import InputFile from '../util/InputFile';
 import RequireKeyMap from '../util/RequireKeyMap';
 
 interface Coordinates { x: number, y: number }
-const MOVE = new RequireKeyMap<string, (c: Coordinates) => Coordinates> ([
-	['R', c => ({ x: c.x + 1, y: c.y + 0 })],
-	['L', c => ({ x: c.x - 1, y: c.y + 0 })],
-	['U', c => ({ x: c.x + 0, y: c.y + 1 })],
-	['D', c => ({ x: c.x + 0, y: c.y - 1 })]
+const MOVE = new RequireKeyMap ([
+	['R', (c: Coordinates) => ({ x: c.x + 1, y: c.y + 0 })],
+	['L', (c: Coordinates) => ({ x: c.x - 1, y: c.y + 0 })],
+	['U', (c: Coordinates) => ({ x: c.x + 0, y: c.y + 1 })],
+	['D', (c: Coordinates) => ({ x: c.x + 0, y: c.y - 1 })]
 ]);
 
-const visited = new Set<string>(['0_0']);
-const knots: Coordinates[] = ArrayUtils.createWith(2, () => ({ x: 0, y: 0 }));
+const visited = new Set(['0_0']);
+const knots = ArrayUtils.createWith(2, () => ({ x: 0, y: 0 }));
 
 InputFile
 	.readLinesForDay(9)
-	.select(line => line.split(' '))
-	.select(words => ({ command: words.get(0), count: parseInt(words.get(1)) }))
-	.selectMany(({ command, count }) => Enumerable.repeat(MOVE.get(command), count))
+	.select(line => line.split(' ').toPair())
+	.selectMany(({ first, second }) => ArrayUtils.create(parseInt(second), MOVE.get(first)))
 	.forEach(command =>
 	{
-		knots[0] = command(knots.get(0));
-		for (let i = 0; i < knots.length - 1; i++)
+		knots[0] = command(knots.getFirst());
+		knots.pairWindows().forEach(pair =>
 		{
-			const first = knots.get(i);
-			const second = knots.get(i + 1);
-			const diffX = first.x - second.x;
-			const diffY = first.y - second.y;
-			const sameColumn = first.x === second.x;
-			const sameRow = first.y === second.y;
+			const diffX = pair.first.x - pair.second.x;
+			const diffY = pair.first.y - pair.second.y;
 			const bigDiffX = Math.abs(diffX) > 1;
 			const bigDiffY = Math.abs(diffY) > 1;
-			const moveDiagonal = !sameRow && !sameColumn && (bigDiffX || bigDiffY);
-			if ((sameRow && bigDiffX) || moveDiagonal)
-				second.x += (diffX > 0) ? 1 : -1;
-			if ((sameColumn && bigDiffY) || moveDiagonal)
-				second.y += (diffY > 0) ? 1 : -1;
-		}
+			const moveDiagonal = diffX && diffY && (bigDiffX || bigDiffY);
+			if ((!diffY && bigDiffX) || moveDiagonal)
+				pair.second.x += (diffX > 0) ? 1 : -1;
+			if ((!diffX && bigDiffY) || moveDiagonal)
+				pair.second.y += (diffY > 0) ? 1 : -1;
+		});
 
-		visited.add(`${knots.getLast().x}_${knots.getLast().y}`);
+		const tail = knots.getLast();
+		visited.add(`${tail.x}_${tail.y}`);
 	});
 
 console.log(visited.size);
