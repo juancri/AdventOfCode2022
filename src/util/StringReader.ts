bf6425bcb9c060f3390058d99eb2ff426fab4375
JC Olivares <juancri@juancri.com>
2022-12-23 21:32:31 -0600

    Adding new method StringReader.nextCheck() Adding new method StringReader.readWhileMatches(exp)

--- src/util/StringReader.ts
+++ src/util/StringReader.ts
@@ -17,11 +17,28 @@ export default class StringReader
 			this.input.at(this.pos++) as string;
 	}
 
+	public nextCheck(): string
+	{
+		const next = this.next();
+		if (next === null)
+			throw new Error(`Reached the end of the string. Expected a character.`);
+		return next;
+	}
+
 	public peek(): string | null
 	{
 		return this.pos >= this.input.length ?
 			null :
 			this.input.at(this.pos) as string;
+	}
+
+	public readWhileMatches(exp: RegExp): string
+	{
+		let result = '';
+		while (this.peek()?.match(exp))
+			result += this.nextCheck();
+
+		return result;
 	}
 
 	public static create(input: string): StringReader
