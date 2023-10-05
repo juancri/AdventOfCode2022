e77ee5779a8b28932fed9cab6bc3cb00f9bb8ddc
JC Olivares <juancri@juancri.com>
2023-10-05 02:23:04 -0300

    Cleaning code

--- src/15/first.ts
+++ src/15/first.ts
@@ -4,7 +4,7 @@ import { readLinesForDay } from '../util/input';
 import { getManhattanDistance, point2DFromArray } from '../util/point2D';
 
 const data = readLinesForDay(15)
-	.select(line => /x=(\d+), y=(\d+): .* x=(-?\d+), y=(-?\d+)$/.exec(line))
+	.select(line => /x=(\d+), y=(\d+): .* x=(-?\d+), y=(-?\d+)/.exec(line))
 	.select(match => match?.slice(1, 5) ?? [])
 	.select(match => match.toIntArray())
 	.select(array => array.toEnumerable())
@@ -14,14 +14,13 @@ const data = readLinesForDay(15)
 	.select(({ first, second }) => ({ sensor: first, beacon: second }))
 	.select(info => ({ ...info, distance: getManhattanDistance(info.beacon, info.sensor) }))
 	.select(info => ({ ...info, distanceY: Math.abs(info.sensor.y - 2_000_000) }))
-	.where(info => info.distanceY <= info.distance)
-	.select(info => ({ ...info, extra: info.distance - info.distanceY }))
+	.where(({ distance, distanceY }) => distanceY <= distance)
 	.toArray();
 
 console.log(data
 	.toEnumerable()
-	.selectMany(info => Enumerable
-		.range(info.sensor.x - info.extra,info.extra * 2 + 1))
+	.select(info => ({ ...info, extra: info.distance - info.distanceY }))
+	.selectMany(({ sensor, extra }) => Enumerable.range(sensor.x - extra, extra * 2 + 1))
 	.distinct()
 	.where(x => !data.some(info => info.beacon.y === 2_000_000 && info.beacon.x === x))
 	.count());
