497fe9a3b50fe00817c4c52293a8ecb764c93357
JC Olivares <juancri@juancri.com>
2023-10-05 02:15:01 -0300

    Adding day 15

---
+++ src/15/first.ts
@@ -0,0 +1,27 @@
+
+import Enumerable from 'linq';
+import { readLinesForDay } from '../util/input';
+import { getManhattanDistance, point2DFromArray } from '../util/point2D';
+
+const data = readLinesForDay(15)
+	.select(line => /x=(\d+), y=(\d+): .* x=(-?\d+), y=(-?\d+)$/.exec(line))
+	.select(match => match?.slice(1, 5) ?? [])
+	.select(match => match.toIntArray())
+	.select(array => array.toEnumerable())
+	.select(list => list.buffer(2))
+	.select(groups => groups.select(point2DFromArray))
+	.select(groups => groups.toArray().toPair())
+	.select(({ first, second }) => ({ sensor: first, beacon: second }))
+	.select(info => ({ ...info, distance: getManhattanDistance(info.beacon, info.sensor) }))
+	.select(info => ({ ...info, distanceY: Math.abs(info.sensor.y - 2_000_000) }))
+	.where(info => info.distanceY <= info.distance)
+	.select(info => ({ ...info, extra: info.distance - info.distanceY }))
+	.toArray();
+
+console.log(data
+	.toEnumerable()
+	.selectMany(info => Enumerable
+		.range(info.sensor.x - info.extra,info.extra * 2 + 1))
+	.distinct()
+	.where(x => !data.some(info => info.beacon.y === 2_000_000 && info.beacon.x === x))
+	.count());
